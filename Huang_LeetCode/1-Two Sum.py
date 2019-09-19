2189836e0c54da1ac4f67b4ebfd19e53f414b3df
huang <1315970725@qq.com>
2019-09-19 18:14:32 +0800

    第五次打卡 1 and 15

---
+++ Huang_LeetCode/1-Two Sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n in enumerate(nums):
+            if n in d: return [d[n], i]
+            d[target - n] = i
