01ccce1f252350f0d9e434cd0481f2c4e9d302f3
uuuque <49139519+uuuque@users.noreply.github.com>
2019-09-23 21:59:50 +0800

    Delete lengthOfLastWord.py

--- lengthOfLastWord.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def lengthoflastword(self, s):
-        return len(s.rstrip().split(" ")[-1])
-
-
-if __name__ == '__main__':
-    a = Solution()
-    sw = input()
-    print(a.lengthoflastword(sw))
