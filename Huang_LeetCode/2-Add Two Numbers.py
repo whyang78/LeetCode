f558d0c7b1def60adf28b1044391b239c9a224bd
huang <1315970725@qq.com>
2019-09-25 14:28:14 +0800

    '第14次打卡2and137'

---
+++ Huang_LeetCode/2-Add Two Numbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode, carry=0) -> ListNode:
+        if not (l1 or l2):
+            return ListNode(1) if carry else None
+        l1, l2 = l1 or ListNode(0), l2 or ListNode(0)
+        val = l1.val + l2.val + carry
+        l1.val, l1.next = val % 10, self.addTwoNumbers(l1.next, l2.next,
+                                                       val > 9)
+        return l1
